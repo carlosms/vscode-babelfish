30d607c0206bfec685015be8d108626ba2cfd46d
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-08-20 11:11:16 +0100

    Set mock uast with postMessage
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

--- ext-src/extension.ts
+++ ext-src/extension.ts
@@ -64,6 +64,31 @@ class ReactPanel {
 					return;
 			}
 		}, null, this._disposables);
+
+		this.sendUAST();
+	}
+
+	public sendUAST() {
+		const someUast = {
+			1: {
+				"InternalType": "CompilationUnit",
+				"StartPosition": { "Offset": 0, "Line": 1, "Col": 1 },
+				"EndPosition": { "Offset": 446, "Line": 0, "Col": 0 },
+				"Roles": ["File"],
+				"Children": [2],
+				expanded: true
+			},
+			2: {
+				"InternalType": "LineComment",
+				"Properties": { "internalRole": "comments", "text": " hello.java" },
+				"StartPosition": { "Offset": 0, "Line": 1, "Col": 1 },
+				"EndPosition": { "Offset": 13, "Line": 1, "Col": 14 },
+				"Roles": ["Comment"],
+				"Children": []
+			}
+		};
+
+		this._panel.webview.postMessage({ uast: someUast });
 	}
 
 	public doRefactor() {
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-/* tslint:disable */
-
-import * as React from 'react';
+import * as React from "react";
 
 import UASTViewer from "uast-viewer";
 import "uast-viewer/dist/default-theme.css";
 
-const someUast = {
-  1: {
-    "InternalType": "CompilationUnit",
-    "StartPosition": { "Offset": 0, "Line": 1, "Col": 1 },
-    "EndPosition": { "Offset": 446, "Line": 0, "Col": 0 },
-    "Roles": ["File"],
-    "Children": [2],
-    expanded: true
-  },
-  2: {
-    "InternalType": "LineComment",
-    "Properties": { "internalRole": "comments", "text": " hello.java" },
-    "StartPosition": { "Offset": 0, "Line": 1, "Col": 1 },
-    "EndPosition": { "Offset": 13, "Line": 1, "Col": 14 },
-    "Roles": ["Comment"],
-    "Children": []
-  }
-};
+interface IState {
+  uast: object;
+}
 
-class App extends React.Component {
+class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      uast: {}
+    };
+
+    window.addEventListener("message", event => {
+      this.setState({
+        uast: event.data.uast
+      });
+    });
+  }
+
   public render() {
-    return <div className="App">
-        <UASTViewer uast={someUast} />
-      </div>;
+    return (
+      <div className="App">
+        <UASTViewer uast={this.state.uast} />
+      </div>
+    );
   }
 }
 
